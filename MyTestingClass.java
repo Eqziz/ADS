c8f0bada3757182845525511ec8c5142d4ae0513
Nodir <142978363+Eqziz@users.noreply.github.com>
2026-05-03 18:28:14 +0500

    Add MyTestingClass with basic structure
    Initial implementation of MyTestingClass with constructor, hashCode, and equals methods.

---
+++ MyTestingClass.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class MyTestingClass {
+    private int id;
+    private String name;
+
+    public MyTestingClass(int id, String name) {}
+
+    @Override
+    public int hashCode() {}
+
+    @Override
+    public boolean equals(Object obj) {}
+
+    public static void main(String[] args) {}
+}
